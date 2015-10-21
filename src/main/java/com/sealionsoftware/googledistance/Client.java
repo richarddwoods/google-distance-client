dc745345ede5b09aa91922c1a0e87c1be8468938
richarddwoods <richarddwoods@gmail.com>
2015-10-21 22:51:35 +1000

    Updated script to add addresses incrementally

--- src/main/java/com/sealionsoftware/googledistance/Client.java
+++ src/main/java/com/sealionsoftware/googledistance/Client.java
@@ -8,31 +8,51 @@ import com.google.maps.model.DistanceMatrixElement;
 import com.google.maps.model.DistanceMatrixRow;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
+import java.util.logging.LogManager;
 
 public class Client {
 
     private static final String KEY_PROPERTY = "google.api.key";
 
+    static {
+        LogManager.getLogManager().reset();
+    }
+
     public static void main(String... args) throws Exception {
 
-        Properties properties = new Properties();
         File directory = new File(System.getProperty("user.home") + File.separator + ".googledistanceclient");
-        File propfile = new File(directory, "runtime.properties");
         if (!directory.exists()) {
             directory.mkdirs();
+        }
+
+        Properties properties = new Properties();
+        File propfile = new File(directory, "runtime.properties");
+        if (propfile.exists()) {
+            properties.load(new FileReader(propfile));
         } else {
-            if (propfile.exists()) {
-                properties.load(new FileReader(propfile));
-            } else {
-                propfile.createNewFile();
+            propfile.createNewFile();
+        }
+
+        Set<String> existingAddresses = new HashSet<>();
+        File addressfile = new File(directory, "addresses.txt");
+        if (addressfile.exists()) {
+            BufferedReader reader = new BufferedReader(new FileReader(addressfile));
+            String address;
+            while ((address = reader.readLine()) != null){
+                existingAddresses.add(address);
             }
+        } else {
+            addressfile.createNewFile();
         }
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
@@ -46,31 +66,58 @@ public class Client {
             GeoApiContext context = new GeoApiContext()
                     .setApiKey(key);
 
-            String[] addresses = readAddresses(reader);
-            DistanceMatrixApiRequest request = DistanceMatrixApi.getDistanceMatrix(context, addresses, addresses);
+            String[] newAddresses = readAddresses(reader);
 
-            DistanceMatrix result = request.await();
-            int i = 0;
-            for (DistanceMatrixRow row : result.rows) {
-                System.out.println("From " + addresses[i] + " (" + result.originAddresses[i++] + ")");
-                int j = 0;
-                for (DistanceMatrixElement element : row.elements) {
-                    System.out.print("\tTo  " + addresses[j++] + ": ");
-                    switch (element.status) {
-                        case NOT_FOUND: {
-                            System.err.println("Route not found");
-                            break;
-                        }
-                        case OK: {
-                            System.out.println(element.distance.humanReadable + ", " + element.duration.humanReadable);
-                        }
-                    }
+            if (existingAddresses.isEmpty()){
+                generateMatrix(context, newAddresses, newAddresses);
+            } else {
+
+                for (String requestedAddress : newAddresses) if (existingAddresses.contains(requestedAddress)) {
+                    throw new RuntimeException("The address " + requestedAddress + " is already in the address file");
+                }
+
+                String[] existingAddressesArray = existingAddresses.toArray(new String[existingAddresses.size()]);
+                String[] combinedAddressArray = new String[newAddresses.length + existingAddressesArray.length];
+                System.arraycopy(newAddresses, 0, combinedAddressArray, 0, newAddresses.length);
+                System.arraycopy(existingAddressesArray, 0, combinedAddressArray, newAddresses.length, existingAddressesArray.length);
+
+                generateMatrix(context, newAddresses, combinedAddressArray);
+                generateMatrix(context, existingAddressesArray, newAddresses);
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(addressfile, true))){
+                for (String newAddress : newAddresses){
+                    bw.write(newAddress);
+                    bw.newLine();
                 }
             }
 
         } catch (Exception e) {
             System.err.println("The request could not be completed: ");
             e.printStackTrace();
+        }
+    }
+
+    private static void generateMatrix(GeoApiContext context, String[] from, String[] to) throws Exception {
+        DistanceMatrixApiRequest request = DistanceMatrixApi.getDistanceMatrix(context, from, to);
+
+        DistanceMatrix result = request.await();
+        int i = 0;
+        for (DistanceMatrixRow row : result.rows) {
+            System.out.println("From " + from[i] + " (" + result.originAddresses[i++] + ")");
+            int j = 0;
+            for (DistanceMatrixElement element : row.elements) {
+                System.out.print("\tTo  " + to[j++] + ": ");
+                switch (element.status) {
+                    case NOT_FOUND: {
+                        System.err.println("Route not found");
+                        break;
+                    }
+                    case OK: {
+                        System.out.println(element.distance.humanReadable + ", " + element.duration.humanReadable);
+                    }
+                }
+            }
         }
     }
 
