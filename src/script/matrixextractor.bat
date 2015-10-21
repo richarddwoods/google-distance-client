3aee59ba662311452d427d1e005e7f937a5bf5c8
richarddwoods <richarddwoods@gmail.com>
2015-10-22 08:10:15 +1000

    Updated bat not to exit on completion

--- src/script/matrixextractor.bat
+++ src/script/matrixextractor.bat
@@ -1,1 +1,1 @@
-java -cp "google-distance-client-1.0-SNAPSHOT.jar;dependency-jars/*" com.sealionsoftware.googledistance.Client
+cmd.exe /k java -cp "google-distance-client-1.0-SNAPSHOT.jar;dependency-jars/*" com.sealionsoftware.googledistance.Client & pause
