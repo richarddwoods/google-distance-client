2eab5b059ead571e8c025f3392cee63242c15c5e
richarddwoods <richarddwoods@gmail.com>
2015-11-01 21:37:35 +1000

    Updated bat to use system proxy and ipv4

--- src/script/matrixextractor.bat
+++ src/script/matrixextractor.bat
@@ -1,1 +1,1 @@
-cmd.exe /k java -cp "google-distance-client-1.0-SNAPSHOT.jar;dependency-jars/*" com.sealionsoftware.googledistance.Client & pause
+cmd.exe /k java -Djava.net.useSystemProxies=true -Djava.net.preferIPv4Stack=true -cp "google-distance-client-1.0-SNAPSHOT.jar;dependency-jars/*" com.sealionsoftware.googledistance.Client & pause
